a5be1cd16981c96d81c20a49b5f004e317e3ac69
Manasseh <91884645+ManassehGit@users.noreply.github.com>
2022-09-14 16:51:52 +0100

    Update index page

--- src/modules/IndexPage/IndexPage.jsx
+++ src/modules/IndexPage/IndexPage.jsx
@@ -5,6 +5,8 @@ const IndexPage = () => {
   return (
     <div id="indexpage">
         <div>
+        <img className='m-3' src='IIOTLogo.png' alt='IIOT Test Bench' width={"200px"}/>
+
         <h1>IIOT Test Bench</h1>
         <p>The heart of a IIoT solution is a message broker</p>
         <Link to={'configconn'}><button type="button" className="btn btn-secondary btn-lg"><i className="fa fa-cog fa-spin fa-fw"></i> Go to Test Bench</button></Link>
